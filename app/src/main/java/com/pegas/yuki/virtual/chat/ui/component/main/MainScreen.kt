3d8a053312900970e81d3e82fc2d3d64a7618c22
anr <longtq2609@gmail.com>
2026-09-14 10:07:32 +0700

    change title topbar

--- app/src/main/java/com/pegas/yuki/virtual/chat/ui/component/main/MainScreen.kt
+++ app/src/main/java/com/pegas/yuki/virtual/chat/ui/component/main/MainScreen.kt
@@ -114,7 +114,7 @@ private fun MainTopBar(
     val coinBalance = viewModel.coinBalance.collectAsStateWithLifecycle().value
 
     CommonTopBar(
-        title = "AI Chat",
+        title = "Yuki",
         coinCount = coinBalance,
         onAddCoinClick = {
             rootNavController.navigate(AppRoutes.MAIN_STORE)
